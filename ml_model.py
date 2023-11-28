1e7876dd9cac9467ae5a53996b026ec30ba27e48
harsha2act <sreeharshac3@gmail.com>
2023-11-28 18:36:02 +0530

    added 3 ML Model

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,12 @@
 print('ML Model file')
+
+def model1():
+    print('adding code for ML model1')
+    
+def model2():
+    print('adding code for ML model2')
+
+def model3():
+    print('adding code for ML model3')
+        
+        
